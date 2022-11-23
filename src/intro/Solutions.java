ec1badc2630a20950ac79c2a958ae2e53a79ce0a
Igor Kang <ikang13@thesmc.co.kr>
2022-11-23 18:50:38 +0900

    knapsack_light_39

--- src/intro/Solutions.java
+++ src/intro/Solutions.java
@@ -42,5 +42,15 @@ public class Solutions {
         }
     }
 
+    public static class knapsack_light_39 {
+        public static int solution(int value1, int weight1, int value2, int weight2, int maxW) {
+            if (weight1 > maxW && weight2 > maxW) return 0;
+            if (weight1 + weight2 <= maxW) return value1 + value2;
+            if (weight1 > maxW) return value2;
+            if (weight2 > maxW) return value1;
+            return Math.max(value1, value2);
+        }
+    }
+
 
 }
