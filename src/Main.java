aa9caab6f52806b4af30cb6bca759e09088a3803
Igor Kang <ikang13@thesmc.co.kr>
2022-11-29 18:35:32 +0900

    longestDigitsPrefix_40 digitDegree_41 bishopAndPawn_42 isBeautifulString_43 findEmailDomain_44 buildPalindrome_45

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import intro.Solutions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solutions.knapsack_light_39.solution(5, 3, 7, 4, 6));
+        System.out.println(Solutions.buildPalindrome_45.mySolution("abcdc"));
     }
 }
--- src/intro/Solutions.java
+++ src/intro/Solutions.java
@@ -1,5 +1,8 @@
 package intro;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solutions {
 
     public static class stringsRearrangement_33 {
@@ -52,5 +55,147 @@ public class Solutions {
         }
     }
 
+    public static class longestDigitsPrefix_40 {
+        public static String solution(String inputString) {
+            StringBuilder ans = new StringBuilder();
+
+            try {
+                String current_symbol = "";
+                for (int i = 0; i < inputString.length(); i++) {
+                    current_symbol = String.valueOf(Integer.parseInt(String.valueOf(inputString.charAt(i))));
+
+                    ans.append(current_symbol);
+                }
+            } catch (NumberFormatException ignored) {
+
+            }
+
+            return ans.toString();
+        }
+
+        public static String best_solution(String inputString) {
+            return inputString.replaceAll("^(\\d*).*", "$1");
+        }
+    }
+
+    public static class digitDegree_41 {
+        public static int mySolution(int n) {
+            int ans = 0;
+            String num = String.valueOf(n);
+            while (num.length() != 1) {
+
+                int sum = 0;
+                for (int i = 0; i < num.length(); i++) {
+                    sum += Integer.parseInt(String.valueOf(num.charAt(i)));
+                }
+                ans++;
+                num = String.valueOf(sum);
+            }
+
+            return ans;
+        }
+
+        public static int solution(int n) {
+            int count = 0;
+            while (n >= 10) {
+                int sum = 0;
+                while (n > 0) {
+                    sum += n % 10;
+                    n /= 10;
+                }
+                n = sum;
+                sum = 0;
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public static class bishopAndPawn_42 {
+        public static boolean mySolution(String bishop, String pawn) {
+            return Math.abs(bishop.charAt(0) - pawn.charAt(0)) == Math.abs(bishop.charAt(1) - pawn.charAt(1));
+        }
+    }
+
+    public static class isBeautifulString_43 {
+        public static boolean mySolution(String inputString) {
+            Map<Character, Integer> map = new HashMap<>() {{
+                put('a', 0);
+                put('b', 0);
+                put('c', 0);
+                put('d', 0);
+                put('e', 0);
+                put('f', 0);
+                put('g', 0);
+                put('h', 0);
+                put('i', 0);
+                put('j', 0);
+                put('k', 0);
+                put('l', 0);
+                put('m', 0);
+                put('n', 0);
+                put('o', 0);
+                put('p', 0);
+                put('q', 0);
+                put('r', 0);
+                put('s', 0);
+                put('t', 0);
+                put('u', 0);
+                put('v', 0);
+                put('w', 0);
+                put('x', 0);
+                put('y', 0);
+                put('z', 0);
+            }};
+
+            for (int i = 0; i < inputString.length(); i++) {
+                map.put(inputString.charAt(i), map.get(inputString.charAt(i)) + 1);
+            }
+            Object[] s = map.values().toArray();
+            for (int i = 0; i < s.length - 1; i++) {
+                if ((Integer) s[i + 1] - (Integer) s[i] > 0) {
+                    return false;
+                }
+            }
+            ;
+
+            return true;
+        }
+
+        public static boolean solution(String s) {
+            int[] c = new int[26];
+
+            for (int i : s.getBytes()) c[i - 'a']++;
+
+            for (int i = 1; i < 26; i++)
+                if (c[i] > c[i - 1]) return false;
+
+            return true;
+        }
+    }
+
+    public static class findEmailDomain_44 {
+        public static String mySolution(String address) {
+            return address.substring(address.lastIndexOf('@') + 1);
+        }
+    }
+    public static class buildPalindrome_45 {
+        public static String mySolution(String st) {
+            StringBuilder initionalPart = new StringBuilder(st);
+            StringBuilder additionalPart = new StringBuilder();
+            for(int i = 0; i < st.length() - 1; ) {
+                if(isPalindrome(initionalPart.toString())) {
+                    break;
+                }
+
+                additionalPart.append(initionalPart.charAt(i));
+                initionalPart.deleteCharAt(i);
+            }
+            return st + additionalPart.reverse();
+        }
+        private static boolean isPalindrome(String st) {
+            return new StringBuilder(st).reverse().toString().equals(st);
+        }
+    }
 
 }
